2da72af5cc5de15a2544e359b08f252f65a43a49
obijuan <obijuan.cube@gmail.com>
2025-06-24 09:58:14 +0200

    add export_sheet.sh script

---
+++ export_sheet.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#-- Comprobar que se le pasa un argumento: nombre de la ficha
+if [ -z "$1" ]; then
+    echo "Error: No se ha especificado el nombre de la ficha."
+    echo "Uso: $0 <nombre_de_la_ficha>"
+    exit 1
+fi
+
+cd Fichas/$1
+
+echo "Cambiando a $PWD"
+
+#-- Exportar a PDF
+echo "Generando... $PWD/$.pdf"
+inkscape $PWD/$1.svg --export-filename=$PWD/$1.pdf
+
+#-- Exportar a PNG
+echo "Generando... $PWD/$1.png"
+inkscape $PWD/$1.svg --export-png=$PWD/$1.png
